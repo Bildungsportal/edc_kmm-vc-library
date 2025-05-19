8ee468552d57ab69738c5d0ba1a56db24b727b32
A-SIT Plus GmbH <plus@a-sit.at>
2025-05-19 12:45:34 +0000

    Mirror latest release

--- vck-openid/src/commonMain/kotlin/at/asitplus/wallet/lib/oidvci/WalletService.kt
+++ vck-openid/src/commonMain/kotlin/at/asitplus/wallet/lib/oidvci/WalletService.kt
@@ -62,11 +62,13 @@ class WalletService(
     private val supportedJweAlgorithm: JweAlgorithm = JweAlgorithm.ECDH_ES,
     /** Algorithm to decrypt credential response encryption, see [requestEncryption]. */
     private val supportedJweEncryptionAlgorithm: JweEncryption = JweEncryption.A256GCM,
+    /** OAuth2 client to build authorization requests */
+    val oauth2Client: OAuth2Client = OAuth2Client(clientId, redirectUrl)
 ) {
 
     data class KeyAttestationInput(val clientNonce: String?, val supportedAlgorithms: Collection<String>?)
 
-    val oauth2Client: OAuth2Client = OAuth2Client(clientId, redirectUrl)
+
 
     data class RequestOptions(
         /**
@@ -261,7 +263,7 @@ class WalletService(
 
     internal suspend fun createCredentialRequestProofAttestation(
         clientNonce: String?,
-        supportedSigningAlgorithms: Collection<String>,
+        supportedSigningAlgorithms: Collection<String>?,
     ): CredentialRequestProof = CredentialRequestProof(
         proofType = ProofType.ATTESTATION,
         attestation = this.loadKeyAttestation?.invoke(KeyAttestationInput(clientNonce, supportedSigningAlgorithms))
